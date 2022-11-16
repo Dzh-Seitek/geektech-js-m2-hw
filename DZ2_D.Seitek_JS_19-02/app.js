be5d028255320236b6e96ef5b0891f9bc7724d20
Dzh-Seitek <82109953+Dzh-Seitek@users.noreply.github.com>
2022-11-16 13:26:17 +0600

    javascript 2-month done hw-2

---
+++ DZ2_D.Seitek_JS_19-02/app.js
@@ -0,0 +1,33 @@
+const block = document.querySelector('.block');
+let posX = 0;
+    posY = 0;
+
+const moveBlock = () => {
+    block.animate([
+        {left: `0px`, top: `0px`},
+        {left: `450px`, top: `0px`},
+        {left: `450px`, top: `450px`},
+        {left: `0px`, top: `450px`},
+        {left: `0px`, top: `0px`}
+    ], {
+        duration: 8000,
+    }, setTimeout(moveBlock, 8000))
+}
+
+window.addEventListener('load', () => {
+    moveBlock();
+})
+
+
+// let count = 0;
+// window.addEventListener('load', (e) => {
+//     const timer = setInterval(() => {
+//         if(count >= 60) {
+//             console.log(`Interval cleared at: ${count}`);
+//             clearInterval(timer);
+//         }else{
+//             count++;
+//             console.log(count);
+//         }
+//     }, 1000);
+// })
